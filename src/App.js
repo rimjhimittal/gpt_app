a6953a5afd31e8cc488c9ca8db31f4c31d331f0f
Rimjhim Mittal <rimjhimittal2003@gmail.com>
2023-01-18 01:08:21 +0530

    rough template

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css'
+//import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import CTA from './components/cta/CTA';
+//import Feature from './components/feature/Feature';
+import Navbar from './components/navbar/Navbar';
+
+import { Footer , Features, Blog, Header, Possibility, WhatGPT3 } from './containers';
+
+
+const App = () => {
+  return (
+    <div className= "App">
+      <div className='gradient_bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
